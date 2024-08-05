9aa0639dc99365f5752bdab6726f605bc31524af
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-05 15:04:25 +0300

    todo app header styling

--- TODO-APP-IMJ/src/assets/components/index.jsx
+++ TODO-APP-IMJ/src/assets/components/index.jsx
@@ -1,7 +1,12 @@
+import styles from "./Header.module.css";
+
 export function Header() {
   return (
-    <header>
-      <p>Header</p>
+    <header className={styles.header}>
+      <p className={styles.p}>
+        <span className={styles.to}>To</span>
+        <span className={styles.do}>Do</span> App
+      </p>
     </header>
   );
 }
